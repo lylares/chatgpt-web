c7c5ae23f49080a1b4997a40a53799fee0b6a11f
lylares <contact@lylares.com>
2024-03-02 01:41:25 +0800

    Update index.vue
    Signed-off-by: lylares <contact@lylares.com>

--- src/components/common/GitHubSite/index.vue
+++ src/components/common/GitHubSite/index.vue
@@ -10,7 +10,7 @@ const releaseVersion = import.meta.env.VITE_RELEASE_VERSION || 'v0.0.0'
     </span>
     <a href="/" target="_blank">
       <span class="text-blue-500">
-        helper
+        ihelper
       </span>
     </a>
     <span v-text="`${releaseVersion}-${gitCommitSha}`" />
