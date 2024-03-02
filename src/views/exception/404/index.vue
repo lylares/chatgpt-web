58f73dd172b43d96484c1c090705ac6a38a54995
lylares <contact@lylares.com>
2024-03-02 14:46:41 +0800

    Update index.vue
    Signed-off-by: lylares <contact@lylares.com>

--- src/views/exception/404/index.vue
+++ src/views/exception/404/index.vue
@@ -5,7 +5,7 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 function goHome() {
-  router.push('/')
+  router.push('/#/chat')
 }
 </script>
 
@@ -24,7 +24,7 @@ function goHome() {
         </div>
       </div>
       <NButton type="primary" @click="goHome">
-        Go to Home
+        回到首页
       </NButton>
     </div>
   </div>
